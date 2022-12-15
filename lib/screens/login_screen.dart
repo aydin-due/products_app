43adfa4a2195f1898ee85e07ed8c67c4e85bfe7f
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-12-15 12:00:38 -0700

    login bg

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:products_app/widgets/widgets.dart';
 
 class LoginScreen extends StatelessWidget {
    
@@ -7,9 +8,7 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(
-         child: Text('LoginScreen'),
-      ),
+      body: AuthBackground()
     );
   }
 }
---
+++ lib/widgets/auth_bg.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class AuthBackground extends StatelessWidget {
+  const AuthBackground({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Stack(children: const [_PurpleBox()]));
+  }
+}
+
+class _PurpleBox extends StatelessWidget {
+  const _PurpleBox({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+
+    return Container(
+      width: double.infinity,
+      height: size.height * 0.4,
+    decoration: _purpleBg(),
+    child: Stack(children: const [
+      Positioned(top: 90, left: 30, child: _Bubble(),),
+      Positioned(top: -40, left: -30, child: _Bubble(),),
+      Positioned(top: -50, right: -20, child: _Bubble(),),
+      Positioned(bottom: -50, left: 10, child: _Bubble(),),
+      Positioned(bottom: 120, right: 20, child: _Bubble(),),
+    ]),
+    );
+  }
+
+  BoxDecoration _purpleBg() => const BoxDecoration(
+    gradient: LinearGradient(colors: [
+      Color.fromRGBO(63, 63, 156, 1),
+      Color.fromRGBO(90, 70, 178, 1)
+    ]),
+  );
+}
+
+class _Bubble extends StatelessWidget {
+  const _Bubble({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(100),
+        color: const Color.fromRGBO(255, 255, 255, 0.05),
+      ),
+    );
+  }
+}
