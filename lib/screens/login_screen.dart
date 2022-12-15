500c008201e52330c3d82968404afe568666a5b3
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-12-15 12:54:29 -0700

    email input

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -13,15 +13,57 @@ class LoginScreen extends StatelessWidget {
           const SizedBox(
             height: 250,
           ),
-          CardContainer(child: Column(
-            children: [
-              SizedBox(height: 10,),
-              Text('Login', style: Theme.of(context).textTheme.headline4,),
-              SizedBox(height: 30,)
-            ],
-          ),)
+          CardContainer(
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Login',
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                const _LoginForm(),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          const Text('Crear una nueva cuenta', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),)
         ]),
       ),
     ));
+  }
+}
+
+class _LoginForm extends StatelessWidget {
+  const _LoginForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Form(child: Column(children: [
+        TextFormField(
+          autocorrect: false,
+          keyboardType: TextInputType.emailAddress,
+          decoration: const InputDecoration(
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.deepPurple),
+            ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.deepPurple, width:2)
+              ),
+              hintText: 'john.doe@gmail.com',
+              labelText: 'Email Address',
+              labelStyle: TextStyle(color: Colors.deepPurple),
+              prefixIcon: Icon(Icons.alternate_email, color: Colors.deepPurple),
+          ),
+        )
+      ],)),
+    );
   }
 }
