8bcc0fd5cfcb6d057dd1ee3bb7e5d4ac44f67cc9
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-12-16 22:44:45 -0700

    consistent input decor

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:products_app/ui/input_decorations.dart';
 import 'package:products_app/widgets/widgets.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -50,19 +51,33 @@ class _LoginForm extends StatelessWidget {
         TextFormField(
           autocorrect: false,
           keyboardType: TextInputType.emailAddress,
-          decoration: const InputDecoration(
-            enabledBorder: UnderlineInputBorder(
-              borderSide: BorderSide(color: Colors.deepPurple),
-            ),
-              focusedBorder: UnderlineInputBorder(
-                borderSide: BorderSide(color: Colors.deepPurple, width:2)
-              ),
-              hintText: 'john.doe@gmail.com',
-              labelText: 'Email Address',
-              labelStyle: TextStyle(color: Colors.deepPurple),
-              prefixIcon: Icon(Icons.alternate_email, color: Colors.deepPurple),
-          ),
-        )
+          decoration: InputDecorations.auth(
+            hintText: 'john.doe@gmail.com', 
+            labelText: 'Email address',
+            prefixIcon: Icons.alternate_email_outlined
+          )
+        ),
+        const SizedBox(height: 30,),
+        TextFormField(
+          obscureText: true,
+          autocorrect: false,
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecorations.auth(
+            hintText: '****',
+            labelText: 'Password',
+            prefixIcon: Icons.lock_outline
+          )
+        ),
+        const SizedBox(height: 30,),
+        MaterialButton(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          disabledColor: Colors.grey,
+          elevation: 0,
+          color: Colors.deepPurple,
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+            child: const Text('Ingresar', style: TextStyle(color: Colors.white),)),
+          onPressed: (){})
       ],)),
     );
   }
---
+++ lib/ui/input_decorations.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:products_app/widgets/auth_bg.dart';
+
+class InputDecorations {
+  static InputDecoration auth({required String hintText, required String labelText, IconData? prefixIcon}) {
+    return InputDecoration(
+      enabledBorder: const UnderlineInputBorder(
+        borderSide: BorderSide(color: Colors.deepPurple),
+      ),
+      focusedBorder: const UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.deepPurple, width: 2)),
+      hintText: hintText,
+      labelText: labelText,
+      labelStyle: const TextStyle(color: Colors.deepPurple),
+      prefixIcon: prefixIcon!=null ? Icon(prefixIcon, color: Colors.deepPurple) : null,
+    );
+  }
+}
