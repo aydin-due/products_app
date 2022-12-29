439e11fa208980c505237d885d0f45d63c5c00e9
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-12-28 18:28:53 -0700

    product card pt. 1

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -8,10 +8,65 @@ class ProductCard extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
       child: Container(
+        decoration: _cardDecoration(),
         margin: const EdgeInsets.only(top: 30, bottom: 50),
         width: double.infinity,
         height: 400,
-        color: Colors.red,
+        child: Stack(
+          alignment: Alignment.bottomLeft,
+          children: const [_BgImage(), _ProductDetails()],
+        ),
+      ),
+    );
+  }
+
+  BoxDecoration _cardDecoration() {
+    return BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(25),
+      boxShadow: const [
+        BoxShadow(
+          color: Colors.black12,
+          offset: Offset(0, 7),
+          blurRadius: 10,
+        ),
+      ],
+    );
+  }
+}
+
+class _ProductDetails extends StatelessWidget {
+  const _ProductDetails({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 70,
+      color: Colors.indigo,
+    );
+  }
+}
+
+class _BgImage extends StatelessWidget {
+  const _BgImage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(25),
+      child: const SizedBox(
+        width: double.infinity,
+        height: 400,
+        child: FadeInImage(
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          image: NetworkImage('https://via.placeholder.com/400x300/f6f6f6'),
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }
