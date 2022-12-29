b6c37ad6504495fb859e91c226819acb7b3f4bed
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-12-29 12:39:44 -0700

    product screen pt 2

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:products_app/ui/input_decorations.dart';
 import 'package:products_app/widgets/widgets.dart';
 
 class ProductScreen extends StatelessWidget {
@@ -12,11 +13,103 @@ class ProductScreen extends StatelessWidget {
         children: [
           Stack(
             children: [
-              ProductImage()
+              const ProductImage(),
+              Positioned(
+                top: 60,
+                left: 20,
+                child: IconButton(
+                    onPressed: () => Navigator.pop(context),
+                    icon: const Icon(
+                      Icons.arrow_back_ios,
+                      color: Colors.white,
+                    )),
+              ),
+              Positioned(
+                top: 60,
+                right: 20,
+                child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.camera_alt_outlined,
+                      color: Colors.white,
+                    )),
+              ),
             ],
+          ),
+          const _ProductForm(),
+          const SizedBox(
+            height: 100,
           )
         ],
       )),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        elevation: 0,
+        backgroundColor: Colors.indigo,
+        child: const Icon(Icons.save_outlined),
+      ),
     );
   }
+}
+
+class _ProductForm extends StatelessWidget {
+  const _ProductForm({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        decoration: _formDecoration(),
+        width: double.infinity,
+        child: Form(
+            child: Column(
+          children: [
+            const SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              decoration: InputDecorations.auth(
+                  hintText: 'Product name', labelText: 'Name:'),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecorations.auth(
+                  hintText: '\$150', labelText: 'Price:'),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            SwitchListTile.adaptive(
+                value: true,
+                title: const Text('Available'),
+                activeColor: Colors.indigo,
+                onChanged: (value) {}),
+            const SizedBox(
+              height: 30,
+            ),
+          ],
+        )),
+      ),
+    );
+  }
+
+  BoxDecoration _formDecoration() => BoxDecoration(
+          color: Colors.white,
+          borderRadius: const BorderRadius.only(
+              bottomLeft: Radius.circular(25),
+              bottomRight: Radius.circular(25)),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black.withOpacity(0.05),
+                offset: const Offset(0, 5),
+                blurRadius: 5)
+          ]);
 }
