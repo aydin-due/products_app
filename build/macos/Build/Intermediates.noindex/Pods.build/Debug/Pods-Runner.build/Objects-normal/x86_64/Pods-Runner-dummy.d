01aa88975c99bf466047a34d76f07e8a7618117f
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2023-02-27 21:43:02 -0700

    config

---
+++ build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/x86_64/Pods-Runner-dummy.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/aydin/dev/flutter\ projects/products_app/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m \
+  /Users/aydin/dev/flutter\ projects/products_app/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap
---
+++ build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/x86_64/Pods_Runner_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/aydin/dev/flutter\ projects/products_app/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
